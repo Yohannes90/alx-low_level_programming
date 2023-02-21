3548e3c86be9cbadd7d68acbf0425bc309b3eb33
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-02-21 20:28:01 +0000

    My first commit for questiom 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer to be added
+ * @b: second integer to be added
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
