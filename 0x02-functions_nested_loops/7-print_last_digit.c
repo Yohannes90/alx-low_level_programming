010487fb0e13767219654519ca3cdc098df843b2
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-02-21 17:32:02 +0000

    My second commit for question 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,12 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = n * -1;
+		last_digit = (-1 * n % 10);
 	}
-	last_digit = n % 10;
+	else
+	{		
+		last_digit = n % 10;
+	}
 	_putchar('0' + last_digit);
 	return (last_digit);
 }
