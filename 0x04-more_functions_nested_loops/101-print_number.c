9a4143b787fe9f41994eaf96350fc12b8af8470f
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-02-23 22:34:00 +0000

    My second commit for question 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,19 +8,14 @@
  */
 void print_number(int n)
 {
-	int k;
-
-	k = n;
 	if (n < 0)
 	{
 		n *= -1;
-		k = n;
 		_putchar('-');
 	}
-	k /= 10;
-	if (k != 0)
+	if (n / 10)
 	{
-		print_number(k);
+		print_number(n / 10);
 	}
-	_putchar((int) n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
