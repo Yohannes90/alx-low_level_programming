3320edcf062ec6f0c11208788f9b3239a1973fe3
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-02-23 22:00:37 +0000

    My first commit for queston 12 (got see about Recursion a bit)

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print number with _putchar
+ * @n: the number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int k;
+
+	k = n;
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+	{
+		print_number(k);
+	}
+	_putchar((int) n % 10 + '0');
+}
