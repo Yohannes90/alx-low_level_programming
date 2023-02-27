9209ab8ca34a9eb991fa34833fb759865916b227
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-02-27 18:38:11 +0000

    My first commit for question 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of string,
+ * starting with the first character
+ * @str: string inpur
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
+
