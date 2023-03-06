ccc39ff4b9bc3aaaacb05abbae550ad0b0d88db4
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-06 13:58:43 +0000

    My first commit for question 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: character to be located
+ *
+ * Return: pointer to the first occerence of c in s, null if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *occerence = '\0';
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			occerence = &s[i];
+			return (occerence);
+		}
+	}
+
+	return (occerence);
+}
