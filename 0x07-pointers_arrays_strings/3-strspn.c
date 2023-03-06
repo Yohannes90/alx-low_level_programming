428f20c4117f719f8d0741a151165884197fa55a
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-06 14:41:13 +0000

    My first commit for question 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to string
+ * @accept: bytes to be checked in s
+ *
+ * Return: number of bytes in s which consist accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, amount;
+
+	amount = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				amount++;
+			}
+		}
+	}
+	return (++amount);
+}
