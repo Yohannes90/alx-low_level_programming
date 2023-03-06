1ed8c1ba891aa615fa2f2408cf7357590d5b68be
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-06 20:23:36 +0000

    My first commit for question 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: pointer to array containing chessboard
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (i < 2 || i > 5)
+			{
+				_putchar(a[i][j]);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
