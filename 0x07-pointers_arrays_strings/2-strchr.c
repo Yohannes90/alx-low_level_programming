609897647e3337b6afdeb5c5abe31e274657c346
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-06 14:09:11 +0000

    My third commit for question 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,16 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *occerence = '\0';
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (s + i);
+			occerence = &s[i];
+			return occerence;
 		}
 	}
 
-	return ('\0');
+	return (occerence);
 }
