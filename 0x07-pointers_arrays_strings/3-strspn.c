148b81917452c72ae74d94952b9321449d48131d
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-06 18:24:49 +0000

    My third commit for question 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,13 +12,14 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j, amount;
 
 	amount = 0;
-	for (i = 0; accept[i]; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; s[j]; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				amount++;
+				break;
 			}
 		}
 	}
