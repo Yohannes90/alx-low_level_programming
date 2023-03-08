7d4b4968a545e9ada1ade63ffa8f13278f9efaab
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-08 14:22:53 +0000

    My firfirst commit for question 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to string to printed
+ * 
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
