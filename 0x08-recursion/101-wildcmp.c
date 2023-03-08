429c75fc2e27b0536115f4759b70c02291fbaec8
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-08 20:43:16 +0000

    My first commit for question 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: 1 if the string can be considerd identical, otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,5 +17,6 @@ int actual_prime(int n, int i);
 int is_palindrome(char *s);
 int check_palindrome(char *s, int i, int length);
 
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
