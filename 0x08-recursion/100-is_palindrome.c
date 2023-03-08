ee354fcf8993778babf7353f919a4d9bb1202a4a
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-08 19:59:58 +0000

    My second commit for question 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return(check_palindrome(s, 0, _strlen_recursion(s)));
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
 }
 
 /**
