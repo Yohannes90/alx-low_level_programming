ce8997ad43bea93d83255e087507101fc48aa758
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-08 17:22:35 +0000

    My second commit for question 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,7 +8,7 @@
  */
 int factorial(int n)
 {
-	if (n == 0 || n== 0)
+	if (n == 0 || n == 0)
 	{
 		return (1);
 	}
@@ -16,7 +16,7 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	return (n * factorial (n - 1));
+	return (n * factorial(n - 1));
 }
 
 
