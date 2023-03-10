bc217ca98b2bf168c3f1e14f294087a5d2006c5a
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-10 19:28:27 +0000

    My third commit for question 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,15 @@ int main(int argc, char *argv[])
 {
 	int product;
 
-	if (argc > 1 && argc < 4)
+	if (argc == 3)
 	{
 		product = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", product);
-		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
