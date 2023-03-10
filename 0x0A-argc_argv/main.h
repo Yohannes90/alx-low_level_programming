09c9e066780f2b94f6140f5c053a7c6005e8a26e
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-10 18:30:36 +0000

    My first commit for question 2

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 int main(__attribute__((unused)) int argc, char *argv[]);
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif /* MAIH_H */
