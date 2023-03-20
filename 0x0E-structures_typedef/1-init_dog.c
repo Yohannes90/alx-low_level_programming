f4eb258d88873c88030389595f0a60e54d77254c
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-20 20:41:31 +0000

    My first commit for question 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initalize a variable of type struct dog
+ * @d: variable to be initalized
+ * @name: name for dog to initalize with
+ * @age: age of dog to initalize with
+ * @owner: name of owner to initalize with
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
