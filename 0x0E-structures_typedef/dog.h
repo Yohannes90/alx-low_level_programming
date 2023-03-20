6d6a6a82be8703f5907ef10a8689db67d35a8a30
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-20 22:29:37 +0000

    My first commit for question 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - Typedef for dog
+ */
+typedef dog dog_t;
 
 #endif /* DOG_H */
