07aa9ad2c400326449064a5ecfb444d1711a360e
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-23 16:33:57 +0000

    My first commit for question 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: calculated answer
  */
-int main(__attribute__((__unused__))int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -22,12 +22,12 @@ int main(__attribute__((__unused__))int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
