e90b75db672535072f743d9c3adafc5cc368ccc6
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-30 16:27:34 +0000

    final for commit correction on question 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *temp = *head;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -24,10 +25,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new_node;
 		return (new_node);
 	}
-	while (head->next)
+	while (temp->next)
 	{
-		*head = (*head)->next;
+		temp = temp->next;
 	}
-	(*head)->next = new_node;
+	temp->next = new_node;
 	return(*head);
 }
