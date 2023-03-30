0376b11962da1c2f26abb50792c2e788e09fb8ca
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-30 17:33:38 +0000

    first commit for question 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void first(void)__attribute__((constructor));
+
+/**
+ * before_main - executes before main does
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
