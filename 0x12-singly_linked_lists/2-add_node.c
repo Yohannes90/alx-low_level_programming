eec1a76cdee53308aa00ef033904213a37be538d
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-30 16:04:48 +0000

    commit for question 2 correction(handled null for malloc)

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,9 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	return (new_node);
+	return (*head);
 }
