811b12bc76d7381f1100d83960bf7630fbb958a8
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-10 17:54:46 +0000

    My first commit for question 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to filename
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it should read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ptr;
+	ssize_t fd, numbyte, t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return 0;
+	}
+	ptr = malloc(sizeof(char) * letters);
+	t = read(fd, ptr, letters);
+	numbyte = write(STDOUT_FILENO, ptr, t);
+	free(ptr);
+	close(fd);
+	return(numbyte);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Exercises on file Input/Output
