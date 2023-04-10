7fb617bd154fdbf75bc7dcfd165c6181b94d0ad8
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-10 20:27:43 +0000

    My second commit for question 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 		}
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC,0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, length);
 	if (fd == -1 || w == -1)
 	{
