ac5663f5a1a5559f70c2d08e47b5138fdd0aee77
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-05-08 15:57:28 +0000

    My first commit for question 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @textcontent: is a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure(if file couldn't be created or written on)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+		{
+			length++;
+		}
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, length);
+	if (fd == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
