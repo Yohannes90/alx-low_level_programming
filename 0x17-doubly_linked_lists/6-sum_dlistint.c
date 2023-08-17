aa2890b566a11cf5bc293086493a6ac8d701b040
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-17 19:23:23 +0300

    first commit for question 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - adde of all the data (n) of a dlistint_t list
+ * @head: head of dlistint_t
+ *
+ * Return: sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	if (current == NULL)
+	{
+		return (sum);
+	}
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
