8cc556239f1a8911f0b26a22875b05b510345425
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-17 16:53:38 +0300

    first commit for question 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements of dlistsint_t list
+ * @h: head of dlistsint
+ *
+ * Return: if NULL 0, or count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while(h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
