1f673daccff815334a0ed42e8ca7db3890e8bb61
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-17 17:20:38 +0300

    second commit for question 1, betty correction

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,8 +3,8 @@
 /**
  * dlistint_len - return number of elements in a lnked dlistint_t list
  * @h: head of dlistint_t
- * 
- * Return: count of elements in dlistint_t 
+ *
+ * Return: count of elements in dlistint_t
 */
 size_t dlistint_len(const dlistint_t *h)
 {
