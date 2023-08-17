d03e188d5c2157647e126f508f27ff397abd9522
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-17 20:37:32 +0300

    third commit for question 7, correction on handling if given empty head

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || h == NULL)
 	{
 		return (NULL);
 	}
