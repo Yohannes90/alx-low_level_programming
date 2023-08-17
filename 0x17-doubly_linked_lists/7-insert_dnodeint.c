9cfa0c47eff1d3f61ff82650e3c0aa0eaf595ae4
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-17 21:44:25 +0300

    seventh commit for question 7, correction on freeing allocated memory

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new_node == NULL || h == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 	new_node->n = n;
@@ -35,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		if (current == NULL)
 		{
-			free(new_node);
+			free(new_node);free(new_node);
 			return (NULL);
 		}
 	}
