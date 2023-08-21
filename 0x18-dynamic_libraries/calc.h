befd47c631d56a8f2cadf790d1f405c891369f7a
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-21 20:12:17 +0300

    adding related files for question 2

---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,10 @@
+#ifndef CALC_H
+#define CALC_H
+
+int add(int op1, int op2);
+int sub(int op1, int op2);
+int mul(int op1, int op2);
+int div(int op1, int op2);
+int mod(int op1, int op2);
+
+#endif /*CALC_H*/
---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,59 @@
+/**
+ * add - calculates sum
+ * @op1: operand 1
+ * @op2: operand 2
+ *
+ * Return: sum
+*/
+int add(int op1, int op2)
+{
+	return (op1 + op2);
+}
+
+/**
+ * sub - calculates difference
+ * @op1: operand 1
+ * @op2: operand 2
+ *
+ * Return: difference
+*/
+int sub(int op1, int op2)
+{
+	return (op1 - op2);
+}
+
+/**
+ * mul - calculates product
+ * @op1: operand 1
+ * @op2: operand 2
+ *
+ * Return: product
+*/
+int mul(int op1, int op2)
+{
+	return (op1 * op2);
+}
+
+/**
+ * div - calculates quotient
+ * @op1: operand 1
+ * @op2: operand 2
+ *
+ * Return: quotient
+*/
+int div(int op1, int op2)
+{
+	return (op1 / op2);
+}
+
+/**
+ * mod - calculates remainder
+ * @op1: operand 1
+ * @op2: operand 2
+ *
+ * Return: remainder
+*/
+int mod(int op1, int op2)
+{
+	return (op1 % op2);
+}
