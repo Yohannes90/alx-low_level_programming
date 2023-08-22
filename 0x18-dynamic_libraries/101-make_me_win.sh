55ff2ec3a882c194cea6020ff76639f3edc3782d
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-22 13:49:56 +0300

    correction for question 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Yohannes90/alx-low_level_programming/master/0x18-dynamic_libraries/random.so
-export LD_PRELOAD=/tmp/random.so
+gcc random.o -fPIC -shared -o random.so
+LD_PRELOAD=$WPD/random.so
