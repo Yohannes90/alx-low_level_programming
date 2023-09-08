e40ed3ef395113f44ffaa25a7f5352cb6d7e105a
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-09 01:50:47 +0300

    first commit for question 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - adds an element to the hash table
+ *@ht: the hash table you want to add or update the key/value to
+ *@key: is the key. key can not be an empty string
+ *@value: is the value associated with the key, can be an empty string
+ *
+ *Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long index, size;
+	hash_node_t *new_node;
+
+	if (ht  == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
