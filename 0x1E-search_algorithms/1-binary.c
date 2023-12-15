cbe1dcd3ce0f00c7d0e1bfc2d758d19711cabbe0
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-12-15 15:22:17 +0300

    second commit for question 1, correction on handling NULL array

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,10 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i, left, right;
 
+    if (array == NULL)
+	{
+		return (-1);
+	}
 	for (left = 0, right = size - 1; right >= left; )
 	{
 		printf("Searching in array: ");
