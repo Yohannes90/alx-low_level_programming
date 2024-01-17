aaf7c011d2a65d6633eb9b4da3c32c27a9de1722
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-01-17 12:22:40 +0300

    updating impotations

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,5 @@
-#include <stddef.h>
-#include <stdio.h>
+#include "search_algos.h"
+
 /**
  * linear_search - search value in an array with Linear search algorithm
  * @array: pointer to the first element of the array to search in
