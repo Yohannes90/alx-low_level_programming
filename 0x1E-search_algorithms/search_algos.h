d02ba420ae5420e6fb4d1399b4d7a1132c926841
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-01-17 19:46:30 +0300

    updating search_algos.h for 105-jump_list.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,7 +3,24 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -13,5 +30,11 @@ int _binary_search(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
+
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /*SEARCH_ALGOS*/
